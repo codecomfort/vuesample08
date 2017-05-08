7ca299c2f4a6e95cd1c9a30af3c53b9ce141d846
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-05-09 01:46:10 +0900

    onSubmitでのemitに反応するイベントハンドラを追加

---
+++ src/GithubOutput/script.js
@@ -0,0 +1,23 @@
+import bus from '../bus';
+
+export default {
+  name: 'GithubOutput',
+  data() {
+    return {
+      currentUsername: null,
+      githubData: {}
+    }
+  },
+  created() {
+    bus.$on('new-username', this.onUsernameChange)
+  },
+  destroyed() {
+    bus.$off('new-username', this.onUsernameChange)
+  },
+  methods: {
+    onUsernameChange(name) {
+      console.log(name);
+      this.currentUsername = name;
+    }
+  }
+}
