7269de2418de1dd2dbb808a50a397adfa4003cc5
codecomfort <hiroyuki.naitou+codecomfort@gmail.com>
2017-05-09 12:08:30 +0900

    ユーザーデータ表示部を別コンポーネントへ切り出し

--- src/GithubOutput/script.js
+++ src/GithubOutput/script.js
@@ -1,8 +1,12 @@
 import bus from '../bus';
 import Vue from 'vue';
+import GithubUserData from '../GithubUserData/index.vue';
 
 export default {
   name: 'GithubOutput',
+  components: {
+    'github-user-data': GithubUserData
+  },
   data() {
     return {
       currentUsername: null,
@@ -25,6 +29,8 @@ export default {
       if (this.githubData.hasOwnProperty(name)) return;
 
       const url = `https://api.github.com/users/${name}`;
+      // 因みにこれが何の問題もなく成功するのは、Github の API が、
+      // 「Access-Control-Allow-Origin: *」をレスポンスヘッダにつけてくれているから。
       fetch(url)
         .then(response => response.json())
         .then(data => {
